f088b954db9f9e006dd55e019dab45818b8fc06e
Farshad Bakhshandegan Moghaddam <f.bakhshandegan@gmail.com>
2016-11-14 12:19:48 +0100

    Refactor data sampler Added 20%RatingsSampledByUser

--- src/main/java/util/Sampler.java
+++ src/main/java/util/Sampler.java
@@ -1,7 +1,6 @@
-/**
- * 
- */
 package util;
+
+import org.apache.log4j.Logger;
 
 import controller.DataLoader;
 import model.DataModel;
@@ -14,21 +13,47 @@ import model.DataModel;
 public class Sampler {
 
     /**
+     * Logger for this class
+     */
+    private static final Logger LOG = Logger.getLogger(Sampler.class.getCanonicalName());
+    
+    /**
      * @param args
      */
     public static
             void main(
                     String[] args)
     {
+        LOG.info("Do not forget to set rating file path to full data");
+        //sampleByRating(20, "20%RatingsSampledByRating.csv");
+        sampleByUser(20, "20%RatingsSampledByUser.csv");
+    }
+
+    /**
+     * Sample data based on rating to the amount of percentage
+     * @param percentage
+     */
+    private static void sampleByRating(final int percentage,final String fileName){        
         final DataLoader loader = new DataLoader();
         final DataModel dataModel = loader.readData();
         dataModel.printStatistic();
 
-        final DataModel sampledDataModel = dataModel.sampleRatings(20);
-        //final DataModel sampledDataModel = dataModel.sampleUsers(20);
+        final DataModel sampledDataModel = dataModel.sampleRatings(percentage);
         sampledDataModel.printStatistic();
-        sampledDataModel.writeRatingsToFile("20%RatingsSampledByRating.csv");
-        //sampledDataModel.writeRatingsToFile("20%RatingsSampledByUser.csv");
+        sampledDataModel.writeRatingsToFile(fileName);
     }
+    
+    /**
+     * Sample data based on user to the amount of percentage
+     * @param percentage
+     */
+    private static void sampleByUser(final int percentage,final String fileName){
+        final DataLoader loader = new DataLoader();
+        final DataModel dataModel = loader.readData();
+        dataModel.printStatistic();
 
+        final DataModel sampledDataModel = dataModel.sampleUsers(percentage);
+        sampledDataModel.printStatistic();
+        sampledDataModel.writeRatingsToFile(fileName);
+    }
 }
