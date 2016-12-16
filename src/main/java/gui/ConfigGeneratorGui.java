af0f7c86dcace67b7ce58943da47a9f7b52e8bd4
Farshad Bakhshandegan Moghaddam <f.bakhshandegan@gmail.com>
2016-12-16 13:21:18 +0100

    Added confirmation dialog

--- src/main/java/gui/ConfigGeneratorGui.java
+++ src/main/java/gui/ConfigGeneratorGui.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
@@ -48,14 +49,14 @@ public class ConfigGeneratorGui extends Application {
 	 * @see javafx.application.Application#start(javafx.stage.Stage)
 	 */
 	@Override
-	public void start(Stage stage) throws Exception {		
+	public void start(Stage stage) throws Exception {
 		showSplash(stage);
-		this.menu=createMenu();		
+		this.menu = createMenu();
 		showMainApplication(stage);
 	}
 
 	/**
-	 * @param stage 
+	 * @param stage
 	 * @return
 	 */
 	private MenuBar createMenu() {
@@ -66,23 +67,23 @@ public class ConfigGeneratorGui extends Application {
 		final MenuItem exit = new MenuItem("Exit");
 		exit.setOnAction(t -> {
 			Platform.exit();
-		});        
+		});
 
-		exit.setAccelerator(new KeyCodeCombination(KeyCode.X,KeyCombination.ALT_DOWN));
+		exit.setAccelerator(new KeyCodeCombination(KeyCode.X, KeyCombination.ALT_DOWN));
 		menuFile.getItems().add(exit);
 
 		final MenuItem aboutUs = new MenuItem("About Us");
 		aboutUs.setOnAction(t -> {
-			creareAboutUs();
-		});        
+			createAboutUs();
+		});
 
 		menuHelp.getItems().addAll(aboutUs);
 
-		menuBar.getMenus().addAll(menuFile,menuHelp);
+		menuBar.getMenus().addAll(menuFile, menuHelp);
 		return menuBar;
 	}
 
-	private void creareAboutUs() {
+	private void createAboutUs() {
 		final Stage newStage = new Stage();
 		final VBox vBox = createAboutUsContent();
 
@@ -91,8 +92,8 @@ public class ConfigGeneratorGui extends Application {
 		newStage.setResizable(false);
 		newStage.setTitle("About Us");
 		vBox.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
-				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: gray;" +
-				"-fx-background: rgb(255, 248, 220);");
+				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: gray;"
+				+ "-fx-background: rgb(255, 248, 220);");
 		newStage.centerOnScreen();
 		newStage.initModality(Modality.APPLICATION_MODAL);
 		newStage.showAndWait();
@@ -109,7 +110,7 @@ public class ConfigGeneratorGui extends Application {
 		link.setOnAction(e -> {
 			getHostServices().showDocument(linkAddress);
 		});
-		final VBox vBox = new VBox(10.0,new Text(content.toString()),link);
+		final VBox vBox = new VBox(10.0, new Text(content.toString()), link);
 		return vBox;
 	}
 
@@ -117,30 +118,74 @@ public class ConfigGeneratorGui extends Application {
 		Platform.runLater(() -> {
 			try {
 				Thread.sleep(2000);
-			} catch (Exception e) {
-				//Not important
+				stage.close();
+			} catch (final Exception exception) {
+				// Not important
 			}
+			showConfirmation();
 			final Stage newStage = new Stage();
 			currentStage = newStage;
 			final BorderPane borderPane = new BorderPane();
 			borderPane.setTop(menu);
 			borderPane.setCenter(new WizardMaker(stage));
-			final Scene scence = new Scene(borderPane,WIDTH,HEIGHT);
+			final Scene scence = new Scene(borderPane, WIDTH, HEIGHT);
 			newStage.setScene(scence);
 			newStage.setResizable(false);
 			newStage.setTitle("Hi-Rec Client");
 			newStage.centerOnScreen();
 			newStage.show();
-			stage.close();
 		});
+	}
+
+	private void showConfirmation() {
+		final Stage newStageConfirmation = new Stage();
+		final VBox vBox = createConfirmationContent(newStageConfirmation);
+
+		final Scene scene = new Scene(vBox);
+		newStageConfirmation.setScene(scene);
+		newStageConfirmation.setResizable(false);
+		newStageConfirmation.initStyle(StageStyle.UNDECORATED);
+		vBox.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
+				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: gray;"
+				+ "-fx-background: rgb(255, 248, 220);");
+		newStageConfirmation.centerOnScreen();
+		newStageConfirmation.initModality(Modality.APPLICATION_MODAL);
+		newStageConfirmation.showAndWait();
+	}
+
+	/**
+	 * @return
+	 */
+	private VBox createConfirmationContent(Stage stage) {
+		final StringBuilder content = new StringBuilder();
+		content.append("This framework developed to be used across with Mise-en-scène Project.").append("\n");
+		content.append(
+				"To acknowledge the use of this recommendation engine in your work, please cite the following paper:")
+				.append("\n");
+		final Hyperlink link = new Hyperlink(
+				"How to Combine Visual Features with Tags to Improve the Movie  Recommendation  Accuracy");
+		link.setVisited(false);
+		final String linkAddress = "https://www.researchgate.net/publication/308780370_How_to_Combine_Visual_Features_with_Tags_to_Improve_Movie_Recommendation_Accuracy";
+		link.setStyle(" -fx-border-color: transparent;-fx-padding: 4 0 4 0;");
+		link.setOnAction(e -> {
+			getHostServices().showDocument(linkAddress);
+		});
+		final CheckBox confiramtionCheckBox = new CheckBox("I accept ");
+		final VBox vBox = new VBox(5.0, new Text(content.toString()), link, confiramtionCheckBox);
+		confiramtionCheckBox.setOnAction(event -> {
+			if (confiramtionCheckBox.isSelected()) {
+				stage.close();
+			}
+		});
+		return vBox;
 	}
 
 	/**
 	 * @param stage
-	 * @throws FileNotFoundException 
-	 * @throws InterruptedException 
+	 * @throws FileNotFoundException
+	 * @throws InterruptedException
 	 */
-	private void showSplash(Stage initStage) throws FileNotFoundException, InterruptedException{
+	private void showSplash(Stage initStage) throws FileNotFoundException, InterruptedException {
 		initStage.centerOnScreen();
 		final URL resource = HiRec.class.getResource("/images/logo.png");
 		final File imageFile = new File(resource.getFile());
@@ -154,7 +199,7 @@ public class ConfigGeneratorGui extends Application {
 		final Scene splashScene = new Scene(splashLayout);
 		initStage.initStyle(StageStyle.UNDECORATED);
 		initStage.setScene(splashScene);
-		initStage.show();	
+		initStage.show();
 	}
 
 	/**
@@ -164,7 +209,9 @@ public class ConfigGeneratorGui extends Application {
 		return currentStage;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javafx.application.Application#stop()
 	 */
 	@Override
