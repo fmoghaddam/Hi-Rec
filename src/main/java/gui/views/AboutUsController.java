54d2efcd6504204d8db4e3e1f9d8ac11c88fd8f0
Hossein Rimaz <17963017+mhrimaz@users.noreply.github.com>
2019-06-28 02:43:55 +0430

    Add AboutUs Controller

---
+++ src/main/java/gui/views/AboutUsController.java
@@ -0,0 +1,38 @@
+package gui.views;
+
+
+import gui.Resetable;
+import javafx.application.HostServices;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AboutUsController implements Initializable, Resetable {
+
+    private HostServices hostServices;
+
+    public void setHostServices(HostServices hostServices) {
+        this.hostServices = hostServices;
+    }
+
+    @FXML
+    public void clickOnHyperlink(ActionEvent event) {
+        String link = ((Hyperlink) event.getSource()).getText();
+        hostServices.showDocument(link);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
